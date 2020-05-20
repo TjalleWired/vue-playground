4a5682906fd9efc2d31e86fa313a2365b1ca8fcf
TjalleWired <65167704+TjalleWired@users.noreply.github.com>
2020-05-20 08:53:57 +0200

    added some things

---
+++ loginfoldertest/src/components/LoginForm.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="LoginForm">
+    <h2>Registration Form</h2>
+
+    <form>
+      <!-- Name -->
+      <label for="fname">First name:</label>
+      <input type="text" id="fname" name="fname" placeholder="John" />
+      <label for="lname">Last name:</label>
+      <input type="text" id="lname" name="lname" placeholder="Doe" />
+      <br />
+      <br />
+
+      <!-- Email and password -->
+      <label for="email">Email:</label>
+      <input type="text" id="email" name="email" placeholder="example@example.com" />
+      <label for="password">Password:</label>
+      <input type="password" name="password" id="password" />
+      <br />
+      <br />
+
+      <!-- Adress Stuff -->
+      <label for="address">Address:</label>
+      <input type="text" id="address" name="address" />
+      <label for="postalcode">Postal Code:</label>
+      <input type="text" id="postalcode" name="postalcode" />
+      <br />
+      <br />
+    </form>
+
+    <button type="submit">Register</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginForm",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+input, form {
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: 200; 
+}
+
+</style>
---
+++ vuetify/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Wiredworkers Logo"
+          class="shrink mr-2"
+          contain
+          src="https://wiredworkers.io/wp-content/uploads/2018/01/cropped-wiredworkers-logo-white-50x40.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <h2>Wiredworkers</h2>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      
+     
+    </v-app-bar>
+
+    <v-content>
+      <LoginForm/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import LoginForm from './components/LoginForm';
+
+export default {
+  name: 'App',
+
+  components: {
+    LoginForm,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style scoped>
+h2 {
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: 100;
+}
+</style>
